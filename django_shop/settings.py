a8eb7d2b3d7c3da56b264fe860f04b5a8e27999b
red-f0x <silverbullpan@gmail.com>
2022-06-13 12:11:04 +0300

    added validation for img and resizing

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -89,11 +89,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_files')
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 AUTH_USER_MODEL = 'users.User'
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from users.models import User
+from django.utils.safestring import mark_safe
 
-admin.site.register(User)
+from users.models import User
+from django import forms
+
+
+class ImageAdminForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['img'].help_text = mark_safe(
+            "<span style='color:red;'> Изображение с разрешением больше 1080х1080 будет обрезано</span>"
+        )
+
+
+class UsersAdmin(admin.ModelAdmin):
+    form = ImageAdminForm
+
+
+admin.site.register(User, UsersAdmin)
